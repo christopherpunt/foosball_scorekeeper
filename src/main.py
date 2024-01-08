dcef6904a6a8c7a28b8a82917b9ec22797a2788c
Punt Chris <chris.punt@tgw-group.com>
2024-01-08 09:31:38 -0500

    allow unsafe added to main run method to run in service

--- src/main.py
+++ src/main.py
@@ -99,4 +99,4 @@ def handleGameCompleted():
     gameManager.gameCompleted()
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5001, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5001, debug=True, allow_unsafe_werkzeug=True)
