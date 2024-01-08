0e4d2850b9f850ecb66cb381706e30c19b683210
Punt Chris <chris.punt@tgw-group.com>
2024-01-08 11:14:44 -0500

    exclude the pycache folder with the sync batch file

--- sync_backend_to_raspi.bat
+++ sync_backend_to_raspi.bat
@@ -1,1 +1,1 @@
-winscp.exe /defaults /synchronize // "C:\workspace\azurerepo\foos_scorecounter\src" /home/wolfgang/foos_backend
+winscp.exe /defaults /synchronize // "C:\workspace\azurerepo\foos_scorecounter\src" /home/wolfgang/foos_backend -filemask="|__pycache__/"
