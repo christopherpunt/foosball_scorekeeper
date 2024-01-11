aff5ea7e2c3e047453f34a6ae65b3977cb288644
Punt Chris <chris.punt@tgw-group.com>
2024-01-11 09:22:34 -0500

    start new thread for sending post requests in the led strip service to keep the UI responsive

--- src/ledStripService.py
+++ src/ledStripService.py
@@ -1,20 +1,22 @@
 import requests
-
+import threading
 
 class LedStripService:
     def __init__(self, url) -> None:
         self.url = url
 
     def sendPostRequest(self, function, data = None):
+        thread = threading.Thread(target=self.makePostRequest, args=(function, data,))
+        thread.start()
+
+    def makePostRequest(self, function, data = None):
         headers = {
             'Content-type': 'application/json',
             'Accept': 'application/json'
         }
         try:
             response = requests.post(self.url + function, json=data, headers=headers)
-            print(f"Status Code: {response.status_code}")
-            print("Response Content:")
-            print(response.text)
+            print(f"Status Code: {response.status_code} with data {response.text}")
         except Exception as e:
             print(f'Could not post to: {self.url + function} with data:{data} error: {e}')
     
