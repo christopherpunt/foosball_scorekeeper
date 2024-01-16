82d093bc06692c298a9cf8bbf3e3bca43cd55a1a
Punt Chris <capunt@tgw-group.com>
2024-01-15 19:15:31 -0500

    add another test

--- test/test_foosballGame.py
+++ test/test_foosballGame.py
@@ -10,3 +10,22 @@ def test_addScoreFrom0():
     assert game.redTeamScore == 1
     assert game.blackTeamScore == 0
 
+    game.addScore('BLACK')
+    assert game.redTeamScore == 1
+    assert game.blackTeamScore == 1
+
+def test_addScorefromNon0():
+    game = FoosballGame("player1", "player2")
+
+    game.redTeamScore = 4
+    game.blackTeamScore = 3
+
+    game.addScore('RED')
+    assert game.redTeamScore == 5
+    assert game.blackTeamScore == 3
+
+    game.addScore('BLACK')
+    assert game.redTeamScore == 5
+    assert game.blackTeamScore == 4
+
+
